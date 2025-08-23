82180ab489ef124008dde466f374cbcec836f6be
harshitaipro <harshit27062002@gmail.com>
2025-08-23 20:05:35 +0530

    Stop tracking database.js

--- src/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const dbConnect =async ()=>{
-    await mongoose.connect("mongodb+srv://harshit27062002:H9J9iFz9mvrBwjKI@cluster0.khc82ot.mongodb.net/harsh");
-
-}
-module.exports=dbConnect;
